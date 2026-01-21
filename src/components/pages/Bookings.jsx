feae3600bfa6cff1aa86c8921794f6850f883c29
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2026-01-21 14:35:34 +0000

    Update Bookings.jsx

--- src/components/pages/Bookings.jsx
+++ src/components/pages/Bookings.jsx
@@ -1,13 +1,26 @@
 import { FiPlus } from "react-icons/fi";
 import BookingsForm from "../ui/BookingsForm";
 import TableList from "../ui/TableList";
+import { useEffect, useState } from "react";
 
 const Bookings = () => {
+
+    const [selectedBooking, setSelectedBooking] = useState(null);
+
+    useEffect(() => {
+        console.log(selectedBooking)
+    }, [selectedBooking])
+
+
     return (
         <div className="text-2xl font-semibold mb-4">
             <p className="text-gray-900 mb-4 dark:text-white">Manage all your bookings here.</p>
-            <TableList />
-            <BookingsForm />
+            <TableList selectedBooking={selectedBooking}
+                setSelectedBooking={setSelectedBooking} />
+            <BookingsForm
+                selectedBooking={selectedBooking}
+                setSelectedBooking={setSelectedBooking}
+            />
         </div>
     )
 }
