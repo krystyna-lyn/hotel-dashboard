c138893e8c9e8a006435ad3af4f95e34f274735e
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2026-01-23 21:54:41 +0000

    Update ActionButtons.jsx

--- src/components/ui/ActionButtons.jsx
+++ src/components/ui/ActionButtons.jsx
@@ -1,10 +1,13 @@
 import { FiEdit, FiTrash } from "react-icons/fi";
 
 
-function ActionButtons() {
+function ActionButtons({ booking, onEdit }) {
+
     return (
         <div className="flex gap-2">
-            <button className="p-2 rounded-md text-gray-600 hover:bg-gray-700 hover:text-white transition">
+            <button
+                onClick={() => onEdit(booking)}
+                className="p-2 rounded-md text-gray-600 hover:bg-gray-700 hover:text-white transition">
                 <FiEdit />
             </button>
             <button className="p-2 rounded-md text-gray-600 hover:bg-gray-700 hover:text-white transition">
