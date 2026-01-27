bd3264ec0929aac88bfbd62cb3bcc01a1c1cb6e4
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2026-01-27 21:46:34 +0000

    Update bookingService.js

--- src/services/bookingService.js
+++ src/services/bookingService.js
@@ -2,10 +2,15 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:5001/bookings';
 
-export const createBooking = (bookingData) => {
-    return axios.post(API_URL, bookingData);
+export const createBooking = (data) => {
+    return axios.post(API_URL, data);
 }
 
 export const getBookings = () => {
     return axios.get(API_URL);
 }
+
+export const updateBooking = (id, data) => {
+    return axios.put(`${API_URL}/${id}`, data);
+}
+
