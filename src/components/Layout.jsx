7778a36fdb405208bb321b21041ed86e739a211c
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2026-03-17 21:42:30 +0000

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,18 +3,18 @@ import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 
 const Layout = () => {
-    return (
-        <div className="flex">
-            <Sidebar />
+  return (
+    <div className="min-h-screen bg-stone-50 dark:bg-stone-900 text-stone-900 dark:text-stone-50 flex">
+      <Sidebar />
 
-            <div className="grow ml-16 md:ml-64 h-full lg:h-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white">
-                <Navbar />
-                <div className="p-6">
-                    <Outlet />
-                </div>
-            </div>
+      <div className="grow ml-16 md:ml-64 flex flex-col">
+        <Navbar />
+        <div className="flex-1 p-4 sm:p-6 lg:p-8">
+          <Outlet />
         </div>
-    );
-};
+      </div>
+    </div>
+  )
+}
 
 export default Layout;
